2664280345546d327ec99c774badd8e8760376e1
Abu Salam[Rack-Server] <abusalamparvezalam@gmail.com>
2023-03-06 11:53:28 +0000

    Updated Routes

--- app/Config/production/Routes.php
+++ app/Config/production/Routes.php
@@ -18,85 +18,132 @@ $routes->setAutoRoute(false);
 // session('has_no_profile') is set at Login Event
 if (session('has_no_profile')) {
 
-	$routes->get('/', 'AccountController::profile', [
-		'as'     => 'profile',
-		'filter' => 'login'
-		]);
-	$routes->post('/', 'AccountController::tryToUpdateProfile', [
-		'as' => 'profile',
-		'filter' => 'login'
-		]);
+  $routes->get('/', 'AccountController::profile', [
+    'as'     => 'profile',
+    'filter' => 'login'
+    ]);
+  $routes->post('/', 'AccountController::tryToUpdateProfile', [
+    'as' => 'profile',
+    'filter' => 'login'
+    ]);
 
 } else {
 
-	$routes->get('/', 'Home::index',[
-		'as' => 'home'
-	]);
-	//$routes->get('ci', 'Home::ci');
+  $routes->get('/', 'Home::index',[
+    'as' => 'home'
+  ]);
+  //$routes->get('ci', 'Home::ci');
 
-	$routes->get('profile', 'AccountController::profile', [
-		'as'     => 'profile',
-		'filter' => 'login'
-		]);
-	
-	// Bookings Route Group
-	$routes->group(
-		'booking',
-		[
-			'namespace' => 'App\Controllers',
-			'filter' => 'login'
-		],
-		function($routes) {
-			
-			// List of All Bookings
-			$routes->get('', 'BookingController::index', [
-				'as' => 'view-bookings',
-				'filter' => 'role:admin'
-				]);
+  $routes->get('profile', 'AccountController::profile', [
+    'as'     => 'profile',
+    'filter' => 'login'
+    ]);
 
-			// Shows the Booking form
-			$routes->get('new', 'BookingController::createBooking', [
-				'as' => 'create-booking',
-				]);
-			$routes->post('new', 'BookingController::tryToCreateBooking', [
-				'as' => 'create-booking'
-				]);
+  // Bookings Route Group
+  $routes->group(
+    'booking',
+    [
+      'namespace' => 'App\Controllers',
+      'filter' => 'login'
+    ],
+    function($routes) {
+      
+      // List of All Bookings
+      $routes->get('', 'BookingController::index', [
+        'as' => 'view-bookings',
+        'filter' => 'role:admin'
+        ]);
 
-			// Make Payment for the booking
-			$routes->get('(:num)/pay', 'BookingController::makePayment/$1', [
-				'as' => 'make-payment',
-				]);
-			$routes->post('(:num)/pay', 'BookingController::tryToMakePayment/$1', [
-				'as' => 'make-payment',
-				]);
-			$routes->get('(:num)/status', 'BookingController::setStatus/$1', [
-				'as' => 'status',
-				]);
-			$routes->post('(:num)/webhook', 'BookingController::webhook/$1', [
-				'as' => 'webhook',
-				]);
-			$routes->get('(:num)/print', 'BookingController::printReceipt/$1', [
-				'as' => 'print',
-				]);
-			$routes->get('check', 'BookingController::getBookingSlot', [
-				'as' => 'check',
-				]);
-			$routes->post('check', 'BookingController::getBookingsByDate', [
-				'as' => 'check',
-				]);
-			$routes->get('ticket', 'BookingController::showTicketSearch', [
-				'as' => 'search',
-				]);
-			$routes->post('ticket', 'BookingController::getBookingsByRef', [
-				'as' => 'search',
-				]);
-			$routes->post('driver', 'BookingController::getDriverDetails', [
-				'as' => 'driver-search',
-				]);
-			$routes->post('crew', 'BookingController::getCrewDetails', [
-				'as' => 'crew-search',
-				]);
-		}
-	);
+      // Shows the Booking form
+      $routes->get('new', 'BookingController::createBooking', [
+        'as' => 'create-booking',
+        ]);
+      $routes->post('new', 'BookingController::tryToCreateBooking', [
+        'as' => 'create-booking'
+        ]);
 
+      // Make Payment for the booking
+      $routes->get('(:num)/pay', 'BookingController::makePayment/$1', [
+        'as' => 'make-payment',
+        ]);
+      $routes->post('(:num)/pay', 'BookingController::tryToMakePayment/$1', [
+        'as' => 'make-payment',
+        ]);
+      $routes->get('(:num)/status', 'BookingController::setStatus/$1', [
+        'as' => 'status',
+        ]);
+      $routes->post('(:num)/webhook', 'BookingController::webhook/$1', [
+        'as' => 'webhook',
+        ]);
+      $routes->get('(:num)/print', 'BookingController::printGatePass/$1', [
+        'as' => 'print',
+        ]);
+      $routes->get('check', 'BookingController::getBookingSlot', [
+        'as' => 'check',
+        ]);
+      $routes->post('check', 'BookingController::getBookingsByDate', [
+        'as' => 'check',
+        ]);
+      $routes->get('reports', 'BookingController::showReportForm', [
+        'as' => 'reports',
+        ]);
+      $routes->post('reports', 'BookingController::getBookingsByDate', [
+        'as' => 'reports',
+        ]);
+
+      $routes->get('daily', 'BookingController::getDailyReport', [
+        'as' => 'daily-report',
+        ]);
+
+      $routes->post('driver', 'BookingController::getDriverDetails', [
+        'as' => 'driver-search',
+        ]);
+      $routes->post('crew', 'BookingController::getCrewDetails', [
+        'as' => 'crew-search',
+        ]);
+    }
+  );
+
+  // Blacklists Route Group
+  $routes->group(
+    'blacklist',
+    [
+      'namespace' => 'App\Controllers',
+      'filter' => 'login'
+    ],
+    function($routes) {
+      
+      // List of All Blacklists
+      $routes->get('report', 'BlacklistController::index', [
+        'as' => 'blacklist-report',
+        ]);
+
+      // List of All Blacklists in PDF
+      $routes->get('', 'BlacklistController::blacklist', [
+        'as' => 'view-blacklists',
+        ]);
+  
+      // Enable Blacklisted Vehicle No / DL No
+      $routes->get('(:num)/enable', 'BlacklistController::enable/$1', [
+        'as' => 'enable-blacklist',
+        ]);
+
+      // Disable Blacklisted Vehicle No / DL No
+      $routes->get('(:num)/disable', 'BlacklistController::disable/$1', [
+        'as' => 'disable-blacklist',
+        ]);
+
+      // Disable Blacklisted Vehicle No / DL No
+      $routes->post('check', 'BlacklistController::check', [
+        'as' => 'check-blacklist',
+        ]);
+
+      $routes->get('new', 'BlacklistController::showBlacklistForm', [
+        'as' => 'create-blacklist',
+        ]);
+      $routes->post('new', 'BlacklistController::tryToBlacklist', [
+        'as' => 'create-blacklist',
+        ]);
+    }
+  );
 }
